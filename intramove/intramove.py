e77e76c3174a04676154fef23e3ea9903d02cc21
Abdellatif Dalab <abdellatif.dalab@repustate.com>
2022-12-24 12:18:15 -0500

    added documentation

--- intramove/intramove.py
+++ intramove/intramove.py
@@ -4,12 +4,15 @@ import webbrowser
 from ratelimiter import RateLimiter
 
 
-
 class Intramove:
-    available_packages = ["headlines-15","headlines-500"]
+    """The Intramove class is a client for interacting with an API that allows you to purchase and use various packages of news headlines analysis services."""
+
+    available_packages = ["headlines-15", "headlines-500"]
     current_service_ip = "http://0.0.0.0:8000"
+
     @classmethod
     def get_available_packages(cls):
+        """The get_available_packages method is a class method that returns a list of available packages that can be purchased."""
         return cls.available_packages
 
     def __init__(self):
@@ -24,51 +27,66 @@ class Intramove:
     def _call_endpoint(self, endpoint, payload):
         try:
             response = requests.get(
-                f"{Intramove.current_service_ip}/{endpoint}", headers=self.headers, params=payload
+                f"{Intramove.current_service_ip}/{endpoint}",
+                headers=self.headers,
+                params=payload,
             )
             response.raise_for_status()
         except requests.exceptions.HTTPError as err:
-            print(f'An HTTP error occurred: {err}')
+            print(f"An HTTP error occurred: {err}")
         except requests.exceptions.RequestException as err:
-            print(f'An error occurred while making the request: {err}')
+            print(f"An error occurred while making the request: {err}")
 
         response = response.json()
 
-        if not response: raise Exception("Client not registered.")
+        if not response:
+            raise Exception("Client not registered.")
 
         return response
 
-    def buy_package(self, product:str="headlines-15", quantity:int=1):
-        assert product in self.packages.keys(), f"Product has to be in {self.packages.keys()}."
+    def buy_package(self, product: str = "headlines-15", quantity: int = 1):
+        """The buy_package method allows you to purchase a package of news headlines by specifying the product name and quantity,
+        and it opens a URL in your web browser to complete the purchase."""
+
+        assert (
+            product in self.packages.keys()
+        ), f"Product has to be in {self.packages.keys()}."
         assert quantity > 0, "Quantity has to be greater than 0."
 
-        payload = {"product_id":self.packages[product], "quantity":quantity}
+        payload = {"product_id": self.packages[product], "quantity": quantity}
         response = requests.post(
-            f"{Intramove.current_service_ip}/checkout", headers=self.headers, params=payload
+            f"{Intramove.current_service_ip}/checkout",
+            headers=self.headers,
+            params=payload,
         )
         url = response.json()["session_id"]["url"]
         webbrowser.open(url)
-    
-    @RateLimiter(max_calls=1, period=0.5)  
-    def get_id(self, email:str, name:str):
-        if not isinstance(email, str) or not email: 
+
+    @RateLimiter(max_calls=1, period=0.5)
+    def get_id(self, email: str, name: str):
+        """The get_id method allows you to retrieve a client ID for a given email and name."""
+
+        if not isinstance(email, str) or not email:
             raise ValueError("email must be a non-empty string")
         if not isinstance(name, str) or not name:
             raise ValueError("name must be a non-empty string")
 
-        payload = {"email": email, "name":name}
+        payload = {"email": email, "name": name}
         response = self._call_endpoint("client_id", payload)
         return response
 
     @RateLimiter(max_calls=1, period=0.5)
-    def get_api_key(self, client_id:str):
+    def get_api_key(self, client_id: str):
+        """The get_api_key method allows you to retrieve an API key for a given client ID."""
         if not isinstance(client_id, str) or not client_id:
             raise ValueError("client_id must be a non-empty string")
 
         try:
             ObjectId(client_id)
         except:
-            raise Exception("Client ID doesn't have the right format, you must use the ID returned.")
+            raise Exception(
+                "Client ID doesn't have the right format, you must use the ID returned."
+            )
 
         payload = {"client_id": client_id}
 
@@ -76,36 +94,52 @@ class Intramove:
         return self.api_key
 
     @RateLimiter(max_calls=1, period=0.5)
-    def credits_available(self, api_key:str):
+    def credits_available(self, api_key: str):
+        """The credits_available method allows you to retrieve information about the number of credits available for a given API key."""
         if not isinstance(api_key, str) or not api_key:
             raise ValueError("api_key must be a non-empty string")
 
         payload = {"api_key": api_key}
         return self._call_endpoint("credits_available", payload)
-    
+
     @RateLimiter(max_calls=1, period=0.5)
-    def credits_consumed(self, api_key:str):
+    def credits_consumed(self, api_key: str):
+        """The credits_consumed method allows you to retrieve information about the number of credits consumed for a given API key."""
         if not isinstance(api_key, str) or not api_key:
             raise ValueError("api_key must be a non-empty string")
         payload = {"api_key": api_key}
         return self._call_endpoint("credits_consumed", payload)
 
     @RateLimiter(max_calls=1, period=0.5)
-    def status(self, api_key:str):
+    def status(self, api_key: str):
+        """The status method allows you to retrieve the status of a given API key."""
         if not isinstance(api_key, str) or not api_key:
             raise ValueError("api_key must be a non-empty string")
         payload = {"api_key": api_key}
         return self._call_endpoint("status", payload)
 
     @RateLimiter(max_calls=1, period=0.0001)
-    def analyze_headline(self, headline:str, date:str, api_key:str, callback_url:str=""):
+    def analyze_headline(
+        self, headline: str, date: str, api_key: str, callback_url: str = ""
+    ):
+        """
+        Analyze a given headline for financial sentiment.
 
+        Parameters:
+        - headline (str): the headline to analyze
+        - date (str): the date of the headline
+        - api_key (str): the API key to use for authentication
+        - callback_url (str, optional): the URL to send the results to. If not provided, the results will be returned directly.
+
+        Returns:
+        - A dictionary containing the results of the analysis.
+        """
         if not isinstance(headline, str) or not headline:
             raise ValueError("headline must be a non-empty string")
-        
+
         if not isinstance(date, str):
             raise ValueError("date must be a string")
-        
+
         if not isinstance(api_key, str) or not api_key:
             raise ValueError("api_key must be a non-empty string")
 
@@ -113,9 +147,9 @@ class Intramove:
             raise ValueError("callback_url must be a string")
 
         headline_payload = {
-        "headline": headline,
-        "date": date,
-        "callback_url": callback_url,
+            "headline": headline,
+            "date": date,
+            "callback_url": callback_url,
         }
 
         headline_headers = {
@@ -130,9 +164,3 @@ class Intramove:
             params=headline_payload,
         )
         return response.json()
-
-
-
-
-    
-
--- pay.py
+++
@@ -1,7 +0,0 @@
-from intramove import Intramove
-
-imove = Intramove()
-
-print(imove.get_available_packages())
-
-imove.buy_package(product="headlines-15", quantity=1)
